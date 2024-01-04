59e34ef1bc9788d1441bb310170123624473d44a
Felix Conesa <felix.conesa@edu.upct.es>
2024-01-04 18:41:45 +0100

    csv random MMSI file implemented for PowerBI

---
+++ flowRefinement/DataToCSVPowerBI.py
@@ -0,0 +1,33 @@
+import json
+import csv
+
+# Ruta del archivo GeoJSON
+geojson_path = r'C:\Users\felix\PycharmProjects\TFG\PruebaFlujoAISData\testRandomMMSI.geojson'
+
+# Ruta del archivo CSV de salida
+csv_path = r'C:\Users\felix\PycharmProjects\TFG\PruebaFlujoAISData\testRandomMMSI.csv'
+
+# Cargar el contenido del archivo GeoJSON
+with open(geojson_path, 'r') as geojson_file:
+    geojson_data = json.load(geojson_file)
+
+# Abrir el archivo CSV para escribir
+with open(csv_path, 'w', newline='') as csv_file:
+    # Crear un objeto escritor CSV con punto y coma como delimitador
+    csv_writer = csv.writer(csv_file, delimiter=';')
+
+    # Iterar sobre las características en el archivo GeoJSON
+    for feature in geojson_data['features']:
+        # Obtener las coordenadas y propiedades
+        coordinates = feature['geometry']['coordinates']
+        timestamp = feature['properties']['timestamp']
+        date = feature['properties']['date']
+
+        # Cambiar el punto por coma en las coordenadas
+        coordinates[0] = str(coordinates[0]).replace('.', ',')
+        coordinates[1] = str(coordinates[1]).replace('.', ',')
+
+        # Escribir una fila en el archivo CSV
+        csv_writer.writerow([coordinates[0], coordinates[1], timestamp, date])
+
+print(f"La conversión se ha completado. El archivo CSV se encuentra en: {csv_path}")
